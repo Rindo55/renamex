00a94eda4728252a5199bafb9e0ac6932c74cd98
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-01-26 15:01:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,42 +1,26 @@
+# GOAL :
+# create config class to hold all config information
+# able to load custom config from env vars
+
 import os
 
-class Config(object):
-    # get a token from @BotFather
-    TG_BOT_TOKEN = "5426835246:AAGpWNE1EQF5_t56BmcdD8QWSJhCtynh3gk"
-    # The Telegram API things
-    APP_ID = "3845818"
-    API_HASH = "95937bcf6bc0938f263fc7ad96959c6d"
-    # Update channel for Force Subscribe
-    UPDATE_CHANNEL = "AnimeHellFire"
-    # log channel
-    #LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
-    # Get these values from my.telegram.org
-    CHAT_ID = "-1001526605992"
-    # Array to store users who are authorized to use the bot
-    AUTH_USERS = "1443454117"
-    # Banned Unwanted Members..
-    BANNED_USERS = []
-    # the download location, where the HTTP Server runs
-    DOWNLOAD_LOCATION = "./DOWNLOADS"
-    # Telegram maximum file upload size
-    MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 2097152000
-    FREE_USER_MAX_FILE_SIZE = 50000000
-    # chunk size that should be used with requests
-    CHUNK_SIZE = "128"
-    # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = "https://placehold.it/90x90"
-    # proxy for accessing youtube-dl in GeoRestricted Areas
-    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
-    HTTP_PROXY = ""
-    # https://t.me/hevcbay/951
-    OUO_IO_API_KEY = ""
-    # maximum message length in Telegram
-    MAX_MESSAGE_LENGTH = 4096
-    # set timeout for subprocess
-    PROCESS_MAX_TIMEOUT = 3600
-    # watermark file
-    DEF_WATER_MARK_FILE = ""
-    # Database url
-    DB_URI = "mongodb+srv://erai:test@cluster0.klm94v8.mongodb.net/?retryWrites=true&w=majority"
+class Config:
+    def __init__(self, custom = {}, prefix = ''):
+        self.prefix = prefix
+        for key in custom:
+            self.__setattr__(key,self.__evar(key, custom[key], bool(custom[key] == None)))
+
+    def __evar(self, name: str, default=None, should_prompt=False):
+        name = self.prefix + name
+        value = os.environ.get(name, default)
+        if not value and should_prompt:
+            try:
+                value = input(f"enter {name}'s value: ")
+            except EOFError:
+                value = default
+            print("\n")
+        return value
     
+    def __iter__(self):
+        for key in self.__dict__:
+            yield (key, self.__dict__[key])
